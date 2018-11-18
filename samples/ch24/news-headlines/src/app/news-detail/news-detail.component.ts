21ee9713cec7d240472239e3ede4fb2ed7ff12bb
waylau <waylau521@gmail.com>
2018-11-19 02:08:09 +0800

    格式化代码

--- samples/ch24/news-headlines/src/app/news-detail/news-detail.component.ts
+++ samples/ch24/news-headlines/src/app/news-detail/news-detail.component.ts
@@ -1,6 +1,6 @@
-import { ActivatedRoute } from '@angular/router';  // 导入路由
+import { ActivatedRoute } from '@angular/router';  // 用于获取路由参数
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser'; // 用于HTML过滤
 import { Location } from '@angular/common'; // 用于回退浏览记录
 
 import { NewsDetailService } from '../news-detail.service';
@@ -15,10 +15,12 @@ export class NewsDetailComponent implements OnInit {
   newsDetailData = null;
   newsUrl = null;
 
-  constructor(private newsDetailService: NewsDetailService, private domSanitizer: DomSanitizer,private route: ActivatedRoute,private location: Location) { }
+  constructor(private newsDetailService: NewsDetailService,
+    private domSanitizer: DomSanitizer,
+    private route: ActivatedRoute,  
+    private location: Location) { }
 
   ngOnInit() {
-
     this.showNewsDetailData();
   }
 
@@ -27,7 +29,8 @@ export class NewsDetailComponent implements OnInit {
     this.route.queryParams.subscribe(p => this.newsUrl = p.newsUrl);// 获取参数
 
     this.newsDetailService.getNewsData(this.newsUrl).subscribe(
-      (newsApiData) => this.newsDetailData = this.domSanitizer.bypassSecurityTrustHtml(newsApiData.toString()) //HTML过滤
+      (newsApiData) => this.newsDetailData =
+        this.domSanitizer.bypassSecurityTrustHtml(newsApiData.toString()) //HTML过滤
     );
   }
 
